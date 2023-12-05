63c6b29354f955ff8a6ec57380bc9d2c408d0263
Anastasya <aamokeeva@yandex.ru>
2023-12-05 19:26:57 +0300

    Добавляет логику на доску задач

--- frontend/src/layouts/AppLayoutHeader.vue
+++ frontend/src/layouts/AppLayoutHeader.vue
@@ -10,11 +10,10 @@
         </div>
 
         <!--    Поиск-->
-        <div class="header__logo">
-            <a href="#" class="logo">
-                <img src="@/assets/img/logo.svg" alt="VueWork logo" width="147" height="23" />
-            </a>
-        </div>
+        <form action="#" class="header__search">
+            <input type="search" name="search" required placeholder="Поиск" />
+            <button type="submit">Найти</button>
+        </form>
 
         <!--    Кнопка создания новой задачи-->
         <a href="#" class="header__create-task">Создать карточку</a>
